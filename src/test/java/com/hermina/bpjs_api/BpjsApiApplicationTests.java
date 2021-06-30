cd88670552d6393d5cd1a35f0a48d3edfbd039bc
mkawanto <mkawanto@gmail.com>
2021-06-30 13:04:02 +0700

    Create BpjsApiApplicationTests.java

---
+++ src/test/java/com/hermina/bpjs_api/BpjsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hermina.bpjs_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpjsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
