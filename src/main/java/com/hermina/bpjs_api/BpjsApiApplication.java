9b2990f06ef03abb74a2cdfd7e0aaac593788a80
mkawanto <mkawanto@gmail.com>
2021-06-30 12:59:15 +0700

    Create BpjsApiApplication.java

---
+++ src/main/java/com/hermina/bpjs_api/BpjsApiApplication.java
@@ -0,0 +1,17 @@
+package com.hermina.bpjs_api;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BpjsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpjsApiApplication.class, args);
+	}
+
+	
+	
+}
