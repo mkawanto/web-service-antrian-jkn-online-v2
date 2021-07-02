daa0935d5873d7580cee6e3e977284ba27f28826
EsaSetiawan <blackmore.star@gmail.com>
2021-07-02 14:53:25 +0700

    Perbaikan (tipedata&fungsi) untuk API Tambah Antrian

--- src/main/java/com/hermina/bpjs_api/controller/BpjsController.java
+++ src/main/java/com/hermina/bpjs_api/controller/BpjsController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.hermina.bpjs_api.model.ParamBatalAntrean;
 import com.hermina.bpjs_api.model.ParamKodeBooking;
 import com.hermina.bpjs_api.model.ParamTambahAntrian;
@@ -34,124 +33,94 @@ import io.swagger.annotations.ApiParam;
 @Api(value = "REST Apis related to Antrian JKN Online versi 2")
 @RestController
 public class BpjsController {
-	
+
 	@Autowired
 	private BpjsService service;
-	
+
 	@Autowired
 	private ExtractHttpRequestHeaderService extractRequestService;
-	
+
 	@ApiOperation(value = "to Add Appointment or Booking to BPJS", response = ParamTambahAntrian.class)
 	@PostMapping("/tambahantrian")
 	@ApiImplicitParams({
-		@ApiImplicitParam(name="X-Cons-ID",paramType = "header",required = true, allowEmptyValue = false, value="X-Cons-ID", example="1565"),
-		@ApiImplicitParam(name="X-Timestamp",paramType = "header",required = true, allowEmptyValue = false, value="X-Timestamp", example="1624525985"),
-		@ApiImplicitParam(name="X-Signature",paramType = "header",required = true, allowEmptyValue = false, value="X-Signature", example="CHzZHTcM7Ga2JD2TheU58+Q1mJ3A7W59K1MzXpTK/D8=")
-	})
+			@ApiImplicitParam(name = "X-Cons-ID", paramType = "header", required = true, allowEmptyValue = false, value = "X-Cons-ID", example = "1565"),
+			@ApiImplicitParam(name = "X-Timestamp", paramType = "header", required = true, allowEmptyValue = false, value = "X-Timestamp", example = "1624525985"),
+			@ApiImplicitParam(name = "X-Signature", paramType = "header", required = true, allowEmptyValue = false, value = "X-Signature", example = "CHzZHTcM7Ga2JD2TheU58+Q1mJ3A7W59K1MzXpTK/D8=") })
 	@Validated
-	public String tambahAntrian(
-			@Valid @RequestBody ParamTambahAntrian tambahAntrian, 
-			HttpServletRequest httpServlet ) {
-		
+	public Object tambahAntrian(@Valid @RequestBody ParamTambahAntrian tambahAntrian, HttpServletRequest httpServlet) {
+
 		HttpHeaders extractHttp = (HttpHeaders) extractRequestService.extracthHttp(httpServlet);
-		return (String) service.tambahAntrian(extractHttp, tambahAntrian);
+		return service.tambahAntrian(extractHttp, tambahAntrian);
 	}
-	
+
 	@ApiOperation(value = "to update Queue Time", response = ParamUpdateWaktuAntrean.class)
 	@PostMapping("/updatewaktuantrian")
 	@ApiImplicitParams({
-		@ApiImplicitParam(name="X-Cons-ID",paramType = "header",required = true, allowEmptyValue = false, value="X-Cons-ID", example="1565"),
-		@ApiImplicitParam(name="X-Timestamp",paramType = "header",required = true, allowEmptyValue = false, value="X-Timestamp", example="1624525985"),
-		@ApiImplicitParam(name="X-Signature",paramType = "header",required = true, allowEmptyValue = false, value="X-Signature", example="CHzZHTcM7Ga2JD2TheU58+Q1mJ3A7W59K1MzXpTK/D8=")
-	})
+			@ApiImplicitParam(name = "X-Cons-ID", paramType = "header", required = true, allowEmptyValue = false, value = "X-Cons-ID", example = "1565"),
+			@ApiImplicitParam(name = "X-Timestamp", paramType = "header", required = true, allowEmptyValue = false, value = "X-Timestamp", example = "1624525985"),
+			@ApiImplicitParam(name = "X-Signature", paramType = "header", required = true, allowEmptyValue = false, value = "X-Signature", example = "CHzZHTcM7Ga2JD2TheU58+Q1mJ3A7W59K1MzXpTK/D8=") })
 	@Validated
-	public Object updateWaktuAntrian(
-			@Valid @RequestBody ParamUpdateWaktuAntrean waktuAntrean,
+	public Object updateWaktuAntrian(@Valid @RequestBody ParamUpdateWaktuAntrean waktuAntrean,
 			HttpServletRequest httpServlet) {
-		
-		
+
 		HttpHeaders extractHttp = (HttpHeaders) extractRequestService.extracthHttp(httpServlet);
-		return  service.updateWaktuAntrian(extractHttp, waktuAntrean);
+		return service.updateWaktuAntrian(extractHttp, waktuAntrean);
 	}
-	
+
 	/* End Point Pembatalan Antrian */
 	@ApiOperation(value = "to cancel Appointment or Booking", response = ParamBatalAntrean.class)
 	@PostMapping("/batalantrian")
 	@ApiImplicitParams({
-		@ApiImplicitParam(name="X-Cons-ID",paramType = "header",required = true, allowEmptyValue = false, value="X-Cons-ID", example="1565"),
-		@ApiImplicitParam(name="X-Timestamp",paramType = "header",required = true, allowEmptyValue = false, value="X-Timestamp", example="1624525985"),
-		@ApiImplicitParam(name="X-Signature",paramType = "header",required = true, allowEmptyValue = false, value="X-Signature", example="CHzZHTcM7Ga2JD2TheU58+Q1mJ3A7W59K1MzXpTK/D8=")
-	})
+			@ApiImplicitParam(name = "X-Cons-ID", paramType = "header", required = true, allowEmptyValue = false, value = "X-Cons-ID", example = "1565"),
+			@ApiImplicitParam(name = "X-Timestamp", paramType = "header", required = true, allowEmptyValue = false, value = "X-Timestamp", example = "1624525985"),
+			@ApiImplicitParam(name = "X-Signature", paramType = "header", required = true, allowEmptyValue = false, value = "X-Signature", example = "CHzZHTcM7Ga2JD2TheU58+Q1mJ3A7W59K1MzXpTK/D8=") })
 	@Validated
-	public Object batalAntrian(
-			@Valid
-			@RequestBody ParamBatalAntrean batalAntrean,
-			HttpServletRequest httpServlet) {
-		
-		
-		HttpHeaders extractHttp = (HttpHeaders) extractRequestService.extracthHttp(httpServlet); 
-				
-		return  service.batalAntrean(extractHttp, batalAntrean);
+	public Object batalAntrian(@Valid @RequestBody ParamBatalAntrean batalAntrean, HttpServletRequest httpServlet) {
+
+		HttpHeaders extractHttp = (HttpHeaders) extractRequestService.extracthHttp(httpServlet);
+
+		return service.batalAntrean(extractHttp, batalAntrean);
 	}
-	
+
 	@ApiOperation(value = "to update Doctor Consultation Schedule", response = ParamUpdateJadwalDokter.class)
 	@PostMapping("/updatejadwaldokter")
 	@ApiImplicitParams({
-		@ApiImplicitParam(name="X-Cons-ID",paramType = "header",required = true, allowEmptyValue = false, value="X-Cons-ID", example="1565"),
-		@ApiImplicitParam(name="X-Timestamp",paramType = "header",required = true, allowEmptyValue = false, value="X-Timestamp", example="1624525985"),
-		@ApiImplicitParam(name="X-Signature",paramType = "header",required = true, allowEmptyValue = false, value="X-Signature", example="CHzZHTcM7Ga2JD2TheU58+Q1mJ3A7W59K1MzXpTK/D8=")
-	})
+			@ApiImplicitParam(name = "X-Cons-ID", paramType = "header", required = true, allowEmptyValue = false, value = "X-Cons-ID", example = "1565"),
+			@ApiImplicitParam(name = "X-Timestamp", paramType = "header", required = true, allowEmptyValue = false, value = "X-Timestamp", example = "1624525985"),
+			@ApiImplicitParam(name = "X-Signature", paramType = "header", required = true, allowEmptyValue = false, value = "X-Signature", example = "CHzZHTcM7Ga2JD2TheU58+Q1mJ3A7W59K1MzXpTK/D8=") })
 	@Validated
-	public Object updateJadwalDokter(
-			@Valid @RequestBody ParamUpdateJadwalDokter updateJadwalDokter,
-			HttpServletRequest httpServlet ) {
-		
-		HttpHeaders extractHttp = (HttpHeaders) extractRequestService.extracthHttp(httpServlet); 
-		return  service.updateJadwalDokter(extractHttp, updateJadwalDokter);
+	public Object updateJadwalDokter(@Valid @RequestBody ParamUpdateJadwalDokter updateJadwalDokter,
+			HttpServletRequest httpServlet) {
+
+		HttpHeaders extractHttp = (HttpHeaders) extractRequestService.extracthHttp(httpServlet);
+		return service.updateJadwalDokter(extractHttp, updateJadwalDokter);
 	}
-	
-	
+
 	@ApiOperation(value = "Doctor Consultation Schedule")
 	@GetMapping("/getjadwaldokter/{kodePoli}/{tanggal}")
 	@ApiImplicitParams({
-		@ApiImplicitParam(name="X-Cons-ID",paramType = "header",required = true, allowEmptyValue = false, value="X-Cons-ID", example="1565"),
-		@ApiImplicitParam(name="X-Timestamp",paramType = "header",required = true, allowEmptyValue = false, value="X-Timestamp", example="1624525985"),
-		@ApiImplicitParam(name="X-Signature",paramType = "header",required = true, allowEmptyValue = false, value="X-Signature", example="CHzZHTcM7Ga2JD2TheU58+Q1mJ3A7W59K1MzXpTK/D8=")
-	})
+			@ApiImplicitParam(name = "X-Cons-ID", paramType = "header", required = true, allowEmptyValue = false, value = "X-Cons-ID", example = "1565"),
+			@ApiImplicitParam(name = "X-Timestamp", paramType = "header", required = true, allowEmptyValue = false, value = "X-Timestamp", example = "1624525985"),
+			@ApiImplicitParam(name = "X-Signature", paramType = "header", required = true, allowEmptyValue = false, value = "X-Signature", example = "CHzZHTcM7Ga2JD2TheU58+Q1mJ3A7W59K1MzXpTK/D8=") })
 	public Object getJadwalDokter(
-			@ApiParam(
-				    name =  "kodePoli",
-				    type = "String",
-				    value = "Polyclinic Code",
-				    example = "ANA",
-				    required = true)
-			@RequestParam String kodePoli,
-			@ApiParam(
-				    name =  "tanggal",
-				    type = "Date",
-				    value = "Consultation Date",
-				    example = "2021-06-21",
-				    required = true)
-			@RequestParam("tanggal") @DateTimeFormat(pattern ="yyyy-MM-dd") LocalDate tanggal,
+			@ApiParam(name = "kodePoli", type = "String", value = "Polyclinic Code", example = "ANA", required = true) @RequestParam String kodePoli,
+			@ApiParam(name = "tanggal", type = "Date", value = "Consultation Date", example = "2021-06-21", required = true) @RequestParam("tanggal") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate tanggal,
 			HttpServletRequest httpServlet) {
-		
+
 		HttpHeaders extractHttp = (HttpHeaders) extractRequestService.extracthHttp(httpServlet);
-		return  service.jadwalDokter(extractHttp,kodePoli, tanggal);
+		return service.jadwalDokter(extractHttp, kodePoli, tanggal);
 	}
-	
+
 	@ApiOperation(value = "get List Task Id")
 	@PostMapping("/getlisttask")
 	@ApiImplicitParams({
-		@ApiImplicitParam(name="X-Cons-ID",paramType = "header",required = true, allowEmptyValue = false, value="X-Cons-ID", example="1565"),
-		@ApiImplicitParam(name="X-Timestamp",paramType = "header",required = true, allowEmptyValue = false, value="X-Timestamp", example="1624525985"),
-		@ApiImplicitParam(name="X-Signature",paramType = "header",required = true, allowEmptyValue = false, value="X-Signature", example="CHzZHTcM7Ga2JD2TheU58+Q1mJ3A7W59K1MzXpTK/D8=")
-	})
-	public Object getListTask(
-			@RequestBody ParamKodeBooking kodeBooking,
-			HttpServletRequest httpServlet) {
-		
+			@ApiImplicitParam(name = "X-Cons-ID", paramType = "header", required = true, allowEmptyValue = false, value = "X-Cons-ID", example = "1565"),
+			@ApiImplicitParam(name = "X-Timestamp", paramType = "header", required = true, allowEmptyValue = false, value = "X-Timestamp", example = "1624525985"),
+			@ApiImplicitParam(name = "X-Signature", paramType = "header", required = true, allowEmptyValue = false, value = "X-Signature", example = "CHzZHTcM7Ga2JD2TheU58+Q1mJ3A7W59K1MzXpTK/D8=") })
+	public Object getListTask(@RequestBody ParamKodeBooking kodeBooking, HttpServletRequest httpServlet) {
+
 		HttpHeaders extractHttp = (HttpHeaders) extractRequestService.extracthHttp(httpServlet);
-		return  service.getListTask(extractHttp, kodeBooking);
+		return service.getListTask(extractHttp, kodeBooking);
 	}
-		
+
 }
--- src/main/java/com/hermina/bpjs_api/service/BpjsService.java
+++ src/main/java/com/hermina/bpjs_api/service/BpjsService.java
@@ -1,7 +1,5 @@
 package com.hermina.bpjs_api.service;
 
-
-
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,99 +17,105 @@ import com.hermina.bpjs_api.model.ParamUpdateWaktuAntrean;
 
 @Service
 public class BpjsService {
-	
+
 	@Autowired
 	ExtractHttpRequestHeaderService url;
 
-	public Object tambahAntrian(HttpHeaders header, ParamTambahAntrian request) {
+	public Object tambahAntrian(HttpHeaders header, ParamTambahAntrian tambahAntrian) {
 		try {
-			
+
 			RestTemplate restTemplate = new RestTemplate();
-						
-			HttpEntity<ParamTambahAntrian> entity = new HttpEntity<ParamTambahAntrian>(request ,header);
-			return restTemplate.exchange( url.getUrl().toString() + "/arsws/rest/v2/antrean/add", HttpMethod.POST, entity, Object.class);
+
+			HttpEntity<ParamTambahAntrian> entity = new HttpEntity<>(tambahAntrian, header);
+			return restTemplate.exchange(url.getUrl().toString() + "/arsws/rest/v2/antrean/add", HttpMethod.POST,
+					entity, Object.class);
 		} catch (Exception e) {
 			// TODO: handle exception
 			return e;
 		}
-		
+
 	}
-	
+
 	public Object updateWaktuAntrian(HttpHeaders header, ParamUpdateWaktuAntrean updateWaktuAntrian) {
-		
+
 		try {
-			
+
 			RestTemplate restTemplate = new RestTemplate();
-						
-			HttpEntity<ParamUpdateWaktuAntrean> entity = new HttpEntity<ParamUpdateWaktuAntrean>(updateWaktuAntrian, header);
-			return restTemplate.exchange( url.getUrl().toString() + "/arsws/rest/v2/antrean/updatewaktu", HttpMethod.POST, entity, Object.class);
+
+			HttpEntity<ParamUpdateWaktuAntrean> entity = new HttpEntity<ParamUpdateWaktuAntrean>(updateWaktuAntrian,
+					header);
+			return restTemplate.exchange(url.getUrl().toString() + "/arsws/rest/v2/antrean/updatewaktu",
+					HttpMethod.POST, entity, Object.class);
 		} catch (Exception e) {
 			// TODO: handle exception
 			return e;
 		}
-		
+
 	}
-	
+
 	public Object batalAntrean(HttpHeaders header, ParamBatalAntrean batalAntrian) {
-	    
+
 		try {
-		
-			RestTemplate restTemplate = new RestTemplate();			
-			HttpEntity<ParamBatalAntrean> entity = new HttpEntity<>(batalAntrian ,header);
-			//System.out.println(entity);
-			
-			return restTemplate.exchange( url.getUrl().toString() + "/arsws/rest/v2/antrean/batal", HttpMethod.POST, entity, Object.class);
-		} catch (Exception e) {
-			// TODO: handle exception
-			//System.out.println("Error");
-			return e;
-		}
-	}
-	
-	public Object updateJadwalDokter(HttpHeaders header,ParamUpdateJadwalDokter updateJadwalDokter) {
-		try {
-			
+
 			RestTemplate restTemplate = new RestTemplate();
-						
-			HttpEntity<ParamUpdateJadwalDokter> entity = new HttpEntity<ParamUpdateJadwalDokter>(updateJadwalDokter ,header);
-			
-					
-			return restTemplate.exchange( url.getUrl().toString() + "/arsws/rest/v1/jadwaldokter/updatejadwaldokter", HttpMethod.POST, entity, Object.class);
+			HttpEntity<ParamBatalAntrean> entity = new HttpEntity<>(batalAntrian, header);
+			// System.out.println(entity);
+
+			return restTemplate.exchange(url.getUrl().toString() + "/arsws/rest/v2/antrean/batal", HttpMethod.POST,
+					entity, Object.class);
+		} catch (Exception e) {
+			// TODO: handle exception
+			// System.out.println("Error");
+			return e;
+		}
+	}
+
+	public Object updateJadwalDokter(HttpHeaders header, ParamUpdateJadwalDokter updateJadwalDokter) {
+		try {
+
+			RestTemplate restTemplate = new RestTemplate();
+
+			HttpEntity<ParamUpdateJadwalDokter> entity = new HttpEntity<ParamUpdateJadwalDokter>(updateJadwalDokter,
+					header);
+
+			return restTemplate.exchange(url.getUrl().toString() + "/arsws/rest/v1/jadwaldokter/updatejadwaldokter",
+					HttpMethod.POST, entity, Object.class);
 		} catch (Exception e) {
 			// TODO: handle exception
 			return e;
 		}
-		
+
 	}
-	
-    public Object jadwalDokter(HttpHeaders header,String kodePoli, LocalDate tanggal) {
-    	try {
-			
+
+	public Object jadwalDokter(HttpHeaders header, String kodePoli, LocalDate tanggal) {
+		try {
+
 			RestTemplate restTemplate = new RestTemplate();
 			HttpEntity<Object> entity = new HttpEntity<>(header);
 
-			return restTemplate.exchange( url.getUrl().toString() +"/arsws/rest/v1/jadwaldokter/kodepoli/"+kodePoli+"/tanggal/"+tanggal, HttpMethod.GET, entity, Object.class);
+			return restTemplate.exchange(url.getUrl().toString() + "/arsws/rest/v1/jadwaldokter/kodepoli/" + kodePoli
+					+ "/tanggal/" + tanggal, HttpMethod.GET, entity, Object.class);
 		} catch (Exception e) {
 			// TODO: handle exception
 			return e;
 		}
-		
+
 	}
-    
-    public Object getListTask(HttpHeaders header, ParamKodeBooking kodeBooking) {
-    	try {
-    		
+
+	public Object getListTask(HttpHeaders header, ParamKodeBooking kodeBooking) {
+		try {
+
 			RestTemplate restTemplate = new RestTemplate();
-						
-			HttpEntity<ParamKodeBooking> entity = new HttpEntity<ParamKodeBooking>(kodeBooking,header);
-			
-			
-			//return entity;
-			return restTemplate.exchange( url.getUrl().toString() + "/arsws/rest/v2/antrean/getlisttask", HttpMethod.POST, entity, Object.class);
+
+			HttpEntity<ParamKodeBooking> entity = new HttpEntity<ParamKodeBooking>(kodeBooking, header);
+
+			// return entity;
+			return restTemplate.exchange(url.getUrl().toString() + "/arsws/rest/v2/antrean/getlisttask",
+					HttpMethod.POST, entity, Object.class);
 		} catch (Exception e) {
 			// TODO: handle exception
 			return e;
 		}
-    	//return kodeBooking;
-    }
+		// return kodeBooking;
+	}
 }
